c81e91ece3c597731e1cf4bb18052614adfb706d
jhoony <webwork.jhoony@gmail.com>
2024-04-30 00:04:30 +0700

    edit .gitignore

--- hw3/config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-module.exports = {
-  API_KEY: process.env.API_KEY || "f889f810c5af463ca4e111529242904",
-  BASE_URL: process.env.BASE_URL || "http://api.weatherapi.com/v1/current.json",
-};
